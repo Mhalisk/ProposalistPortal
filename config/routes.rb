8ecae1d5785a3ba65f65f6cd15f4ac26e6c200b2
mhalisk <halisky.michael@gmail.com>
2018-02-08 15:40:57 -0700

    Removed pages and fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,6 @@ Rails.application.routes.draw do
 
   # Encor Solar Routes
   
-  get 'encor_solar/locations'
-  get 'encor_solar/arizona'
-  get 'encor_solar/california_north'
-  get 'encor_solar/california_south'
-  get 'encor_solar/connecticut'
-  get 'encor_solar/florida'
-  get 'encor_solar/massachusetts'
-  get 'encor_solar/nevada'
-  get 'encor_solar/new_york_long_island'
-  get 'encor_solar/new_york_nyc_boroughs'
-  get 'encor_solar/rhode_island'
-  get 'encor_solar/texas_austin_energy_oncor'
-  get 'encor_solar/texas_san_antonio_cps'
-  get 'encor_solar/utah'
+  get 'encor_solar/home'
+
 end

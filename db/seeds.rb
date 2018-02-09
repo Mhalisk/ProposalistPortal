879bf968f61effe259b1df2cc05e20f315dd5180
mhalisk <halisky.michael@gmail.com>
2018-02-09 16:26:39 -0700

    Changed creation order in sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# Users
-
-1.times do |user|
-  User.create!(email: "admin@encorsolar.com", password: ENV["ADMIN_PASSWORD"], roles: "admin")
-end
-
-puts '1 admin user created'
-
 # Companies
 
 1.times do |company|
@@ -23,22 +15,30 @@ puts "Company Powerhome Solar was created"
 # Locations
 
 locations_arr = ['Arizona', 
-                 'California (North)', 
-                 'California (South)', 
-                 'Connecticut', 
-                 'Florida', 
-                 'Massachusetts', 
-                 'Nevada', 
-                 'New York (Long Island)', 
-                 'New York (NYC | The Boroughs)', 
-                 'Rhode Island', 
-                 'Texas (Austin Energy | Oncor)', 
-                 'Texas (San Antonio | CPS)', 
-                 'Utah'
-                ]
+'California (North)', 
+'California (South)', 
+'Connecticut', 
+'Florida', 
+'Massachusetts', 
+'Nevada', 
+'New York (Long Island)', 
+'New York (NYC | The Boroughs)', 
+'Rhode Island', 
+'Texas (Austin Energy | Oncor)', 
+'Texas (San Antonio | CPS)', 
+'Utah'
+]
 
 13.times do |location|
   Location.create!(location: locations_arr[location])
 end
 
 puts "13 locations have been created"
+
+# Users
+
+1.times do |user|
+  User.create!(email: "admin@encorsolar.com", password: ENV["ADMIN_PASSWORD"], roles: "admin")
+end
+
+puts '1 admin user created'

911aba0135066aec82b6471767621e74a3844de7
mhalisk <halisky.michael@gmail.com>
2018-02-14 11:38:24 -0700

    Added render in encor_dividend

--- app/controllers/concerns/encor_dividend.rb
+++ app/controllers/concerns/encor_dividend.rb
@@ -2,6 +2,7 @@ module EncorDividend
   extend ActiveSupport::Concern
 
   def run_encor_dividend
+    render :nothing => true
     `cd #{Rails.root} && rake encor_solar:dividend --trace`
     puts "You have ran run_encor_dividend"
   end

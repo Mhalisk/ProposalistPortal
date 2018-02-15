0e9d13b6ba54681540c350e5981badbf8fdc1b1c
mhalisk <halisky.michael@gmail.com>
2018-02-15 13:20:46 -0700

    Implemented delayed_jobs gem and refactored rake task into a job

--- app/controllers/concerns/encor_dividend.rb
+++ app/controllers/concerns/encor_dividend.rb
@@ -1,10 +1,10 @@
 module EncorDividend
   extend ActiveSupport::Concern
 
-  def run_encor_dividend
-    render 'encor_solar/run_encor_dividend.html.erb'
-    puts "Custom render was rendered"
-    `cd #{Rails.root} && rake encor_solar:dividend --trace`
+  def run_encor_dividend()
+    # Uncomment command below to run as rake task
+    # `cd #{Rails.root} && rake encor_solar:dividend`
+    EncorSolarDividendJob.perform_later()
   end
 
 end

58c9cb13c92ab48e55238f1731431d9860647798
mhalisk <halisky.michael@gmail.com>
2018-03-30 13:02:55 -0600

    Configured chromedriver to run headless.

--- app/jobs/encor_solar_dividend_job.rb
+++ app/jobs/encor_solar_dividend_job.rb
@@ -29,9 +29,9 @@ class EncorSolarDividendJob < ApplicationJob
     # To switch between normal and headless add/remove headless option from array and add/remove headless part of :headless_chrome
     chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
     options = {}
-    options[:args] = ['disable-gpu', 'window-size=1280,1024']
+    options[:args] = ['headless', 'disable-gpu', 'window-size=1280,1024']
     options[:binary] = chrome_bin if chrome_bin
-    Capybara.register_driver :chrome do |app|
+    Capybara.register_driver :headless_chrome do |app|
       Capybara::Selenium::Driver.new(app,
          browser: :chrome,
          options: Selenium::WebDriver::Chrome::Options.new(options)
